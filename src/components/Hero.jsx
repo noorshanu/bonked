acadc4617959da543a24402b1ad91d1a2c9c388f
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-02-04 02:39:32 +0530

    new

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 function Hero() {
   return (
-    <section className="hero-main relative">
-      <div className=" container-wrapper pt-[25%] sm:pt-[10%]">
+    <section className="hero-main relative overflow-x-hidden bg-[#ffbe2d] ">
+      <img src="images/bg-main.png" alt="" className="anime2  overflow-x-hidden " />
+      <div className=" container-wrapper pt-[25%] sm:pt-[10%] ">
         <div>
           <img src="images/hero-txt.png" alt="" className=" mx-auto h-auto sm:h-[200px] w-auto my-[1.5%]" />
 
