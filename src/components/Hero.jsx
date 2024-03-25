fb59215f7d1a427b203099c50546600624147a34
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-03-25 14:34:06 +0530

    mmm

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Hero() {
   return (
     <section className="hero-main relative overflow-x-hidden bg-[#ffbe2d] ">
-      <img src="images/bg-main.png" alt="" className="anime2  overflow-x-hidden " />
+  
       <div className=" container-wrapper pt-[25%] sm:pt-[10%] ">
         <div>
           <img src="images/hero-txt.png" alt="" className=" mx-auto h-auto sm:h-[200px] w-auto my-[1.5%]" />
@@ -12,7 +12,7 @@ function Hero() {
         </div>
       </div>
       <img src="images/hero.png" alt="" className=" mx-auto h-auto sm:h-[350px]" />
-      <img src="images/hero-btn.png" alt="" className=" mx-auto absolute left-0 right-0 top-[80%] sm:top-[70%] h-[30px] sm:h-[80px] " />
+     <a href="/">DexTools</a>
     </section>
   );
 }
