b08270a1b5d16de0a887237bd4f121132623652c
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-03-26 19:46:56 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,6 +14,7 @@ function Hero() {
       <img src="images/hero.png" alt="" className=" mx-auto h-auto sm:h-[350px]" />
       {/* <img src="images/hero-btn.png" alt="" className="  h-[30px] sm:h-[80px] " /> */}
       <a href="https://www.dextools.io/app/en/solana/pair-explorer/3m7oTxgZnsKMQrYQwgEEf1detZk6gcEthJKrqtYf6wxu?t=1711451211650" className=" mx-auto absolute left-0 right-0 top-[80%] sm:top-[70%] text-xl text-black bg-white hover:bg-[#1a4093] rounded-full p-2 border-2 border-[#000]  hover:text-[#fff] w-[200px] text-center font-bold uppercase"> DexTools</a>
+    <a href="https://solscan.io/token/BSFjg8wpS8ZNvn4us4jwARw6XhxnGqE7z2EwDohR1Thh" className="text-sm sm:text-2xl font-bold text-center"> BSFjg8wpS8ZNvn4us4jwARw6XhxnGqE7z2EwDohR1Thh</a>
     </section>
   );
 }
